0a8ca20b6c0f06231a1153410d80ab5144137c4f
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-10-30 21:14:08 +0000

    Corrected title bar text.

--- lib/main.rb
+++ lib/main.rb
@@ -73,7 +73,7 @@ class GameWindow < Chingu::Window
     @pixel = Image.create 1, 1
     @pixel.clear color: :white
 
-    self.caption = "Smash and Grab - By Spooner [Escape - reset turn; F5/F6 - quicksave/load; Ctrl-z/y - undo/redo]"
+    self.caption = "Smash and Grab - By Spooner [Escape - end turn; F5/F6 - quicksave/load; Ctrl-z/y - undo/redo; Arrows - scroll map]"
 
     self.cursor = true
     push_game_state World
