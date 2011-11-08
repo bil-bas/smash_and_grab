07a3e8fdae41cd9d0a2af617e4662b42ec871d82
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-11-08 14:48:26 +0000

    Filter out dead AI entities so they don't fail to move in the turn.

--- lib/players/player.rb
+++ lib/players/player.rb
@@ -11,7 +11,7 @@ class Player
     @faction.player = self
 
     @faction.subscribe :turn_started do |faction, entities|
-      @active_entities = entities.shuffle
+      @active_entities = entities.select(&:alive?).shuffle
     end
 
     @faction.subscribe :turn_ended do
@@ -37,7 +37,6 @@ end
 class Player::AI < Player
   def update
     if @active_entities.empty?
-      @active_entities = nil
       faction.end_turn
     else
       # Attempt to attack, else move, else stand around like a loon.
@@ -46,13 +45,18 @@ class Player::AI < Player
         moves, attacks = entity.potential_moves.partition {|t| t.empty? }
 
         if attacks.any?
-          # Todo: Pick the nearest attack and consider re-attacking.
+          # TODO: Pick the nearest attack and consider re-attacking.
           path = entity.path_to(attacks.sample)
           faction.map.actions.do :move, path.previous_path if path.requires_movement?
           faction.map.actions.do :melee, path
           @active_entities.shift unless entity.ap > 0
         elsif moves.any?
+          # TODO: Wait with moves until everyone who can has attacked?
           faction.map.actions.do :move, entity.path_to(moves.sample)
+          @active_entities.shift
+        else
+          # Can't do anything at all :(
+          # TODO: Maybe wait until other people have tried to move?
           @active_entities.shift
         end
       end
