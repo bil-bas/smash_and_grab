481cacfe5a4c9524c1934ae0be3df6efccf79249
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-11-09 15:39:36 +0000

    Spruced up the main menu a little bit.

--- lib/states/main_menu.rb
+++ lib/states/main_menu.rb
@@ -2,17 +2,40 @@ class MainMenu < Fidgit::GuiState
   def setup
     super
 
+    @container.background_color = Color.rgb(0, 0, 25)
+
     vertical align: :center do
-      button "Play" do
-        push_game_state PlayLevel
+      horizontal align: :center do
+        image_frame StaticObject.sprites[1, 0], factor: 3
+
+        vertical align: :center do
+          label "Smash", font_height: 100, align: :center, color: Color::RED
+          label "and", font_height: 60, align: :center, padding_top: -50, color: Color.rgb(200, 200, 200)
+          label "Grab", font_height: 100, align: :center, padding_top: -45, color: Color::RED
+        end
+
+        image_frame StaticObject.sprites[1, 0], factor: 3
       end
 
-      button "Edit" do
-        push_game_state EditLevel
-      end
+      horizontal align: :center do
+        image_frame Entity.sprites[3, 1], factor: 4
 
-      button "Quit" do
-        exit
+        vertical align: :center do
+          options = { width: 120, justify: :center }
+          button "Play", options do
+            push_game_state PlayLevel
+          end
+
+          button "Edit", options do
+            push_game_state EditLevel
+          end
+
+          button "Quit", options do
+            exit
+          end
+        end
+
+        image_frame Entity.sprites[7, 3], factor: 4
       end
     end
   end
