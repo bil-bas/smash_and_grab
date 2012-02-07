241dafa07cf597fd79f0f5d9b85fa3fa2fe2d4ce
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2012-02-07 06:29:17 +0000

    Pulled some code out of the bloated Entity class.

---
+++ lib/smash_and_grab/mixins/pathfinding.rb
@@ -0,0 +1,126 @@
+require_relative "../path"
+
+module SmashAndGrab
+  module Mixins
+    module Pathfinding
+      # Returns a list of tiles this entity could move to (including those they could melee at) [Set]
+      def potential_moves
+        destination_tile = tile # We are sort of working backwards here.
+
+        # Tiles we've already dealt with.
+        closed_tiles = Set.new
+        # Tiles we've looked at and that are in-range.
+        valid_tiles = Set.new
+        # Paths to check { tile => path_to_tile }.
+        open_paths = { destination_tile => Paths::Start.new(destination_tile, destination_tile) }
+
+        melee_cost = has_ability?(:melee) ? ability(:melee).action_cost : Float::INFINITY
+
+        while open_paths.any?
+          path = open_paths.each_value.min_by(&:cost)
+          current_tile = path.last
+
+          open_paths.delete current_tile
+          closed_tiles << current_tile
+
+          exits = current_tile.exits(self).reject {|wall| closed_tiles.include? wall.destination(current_tile) }
+          exits.each do |wall|
+            testing_tile = wall.destination(current_tile)
+            object = testing_tile.object
+
+            if object and object.is_a?(Objects::Entity) and enemy?(object)
+              # Ensure that the current tile is somewhere we could launch an attack from and we could actually perform it.
+              if (current_tile.empty? or current_tile == tile) and ap >= melee_cost
+                valid_tiles << testing_tile
+              end
+
+            elsif testing_tile.passable?(self) and (object.nil? or object.passable?(self))
+              new_path = Paths::Move.new(path, testing_tile, wall.movement_cost)
+
+              # If the path is shorter than one we've already calculated, then replace it. Otherwise just store it.
+              if new_path.move_distance <= movement_points
+                old_path = open_paths[testing_tile]
+                if old_path
+                  if new_path.move_distance < old_path.move_distance
+                    open_paths[testing_tile] = new_path
+                  end
+                else
+                  open_paths[testing_tile] = new_path
+                  valid_tiles << testing_tile if testing_tile.empty?
+                end
+              end
+            end
+          end
+        end
+
+        valid_tiles
+      end
+
+      # A* path-finding.
+      def path_to(destination_tile)
+        return Paths::None.new if destination_tile == tile
+        return Paths::Inaccessible.new(destination_tile) unless destination_tile.passable?(self)
+
+        closed_tiles = Set.new # Tiles we've already dealt with.
+        open_paths = { tile => Paths::Start.new(tile, destination_tile) } # Paths to check { tile => path_to_tile }.
+
+        destination_object =  destination_tile.object
+        destination_is_enemy = (destination_object and destination_object.is_a? Objects::Entity and destination_object.enemy?(self))
+
+        melee_cost = has_ability?(:melee) ? ability(:melee).action_cost : Float::INFINITY
+
+        while open_paths.any?
+          # Check the (expected) shortest path and move it to closed, since we have considered it.
+          path = open_paths.each_value.min_by(&:cost)
+          current_tile = path.last
+
+          return path if current_tile == destination_tile
+
+          open_paths.delete current_tile
+          closed_tiles << current_tile
+
+          next if path.is_a? Paths::Melee
+
+          # Check adjacent tiles.
+          exits = current_tile.exits(self).reject {|wall| closed_tiles.include? wall.destination(current_tile) }
+          exits.each do |wall|
+            testing_tile = wall.destination(current_tile)
+
+            new_path = nil
+
+            object = testing_tile.object
+            if testing_tile.zoc?(faction) and not (testing_tile == destination_tile or destination_is_enemy)
+              # Avoid tiles that have zoc, unless at the end of the path. You have to MANUALLY enter.
+              next
+            elsif object and object.is_a?(Objects::Entity) and enemy?(object)
+              # Ensure that the current tile is somewhere we could launch an attack from and we could actually perform it.
+              if (current_tile.empty? or current_tile == tile) and ap >= melee_cost
+                new_path = Paths::Melee.new(path, testing_tile)
+              else
+                next
+              end
+            elsif testing_tile.passable?(self)
+              if object.nil? or object.passable?(self)
+                new_path = Paths::Move.new(path, testing_tile, wall.movement_cost)
+              else
+                next
+              end
+            end
+
+            # If the path is shorter than one we've already calculated, then replace it. Otherwise just store it.
+            old_path = open_paths[testing_tile]
+            if old_path
+              if new_path.move_distance < old_path.move_distance
+                open_paths[testing_tile] = new_path
+              end
+            else
+              open_paths[testing_tile] = new_path
+            end
+          end
+        end
+
+        Paths::Inaccessible.new(destination_tile) # Failed to connect at all.
+      end
+    end
+  end
+end
--- lib/smash_and_grab/objects/entity.rb
+++ lib/smash_and_grab/objects/entity.rb
@@ -1,15 +1,20 @@
 require 'set'
 require 'fiber'
 
-require_relative "../path"
+
 require_relative "../abilities"
 require_relative "world_object"
 require_relative "floating_text"
+
+require_relative "../mixins/line_of_sight"
+require_relative "../mixins/pathfinding"
 
 module SmashAndGrab
 module Objects
 class Entity < WorldObject
   extend Forwardable
+  include Mixins::LineOfSight
+  include Mixins::Pathfinding
 
   CLASS = :entity
 
@@ -269,125 +274,6 @@ class Entity < WorldObject
     super
   end
 
-  # Returns a list of tiles this entity could move to (including those they could melee at) [Set]
-  def potential_moves
-    destination_tile = tile # We are sort of working backwards here.
-
-    # Tiles we've already dealt with.
-    closed_tiles = Set.new
-    # Tiles we've looked at and that are in-range.
-    valid_tiles = Set.new
-    # Paths to check { tile => path_to_tile }.
-    open_paths = { destination_tile => Paths::Start.new(destination_tile, destination_tile) }
-
-    melee_cost = has_ability?(:melee) ? ability(:melee).action_cost : Float::INFINITY
-
-    while open_paths.any?
-      path = open_paths.each_value.min_by(&:cost)
-      current_tile = path.last
-
-      open_paths.delete current_tile
-      closed_tiles << current_tile
-
-      exits = current_tile.exits(self).reject {|wall| closed_tiles.include? wall.destination(current_tile) }
-      exits.each do |wall|
-        testing_tile = wall.destination(current_tile)
-        object = testing_tile.object
-
-        if object and object.is_a?(Objects::Entity) and enemy?(object)
-          # Ensure that the current tile is somewhere we could launch an attack from and we could actually perform it.
-          if (current_tile.empty? or current_tile == tile) and ap >= melee_cost
-            valid_tiles << testing_tile
-          end
-
-        elsif testing_tile.passable?(self) and (object.nil? or object.passable?(self))
-          new_path = Paths::Move.new(path, testing_tile, wall.movement_cost)
-
-          # If the path is shorter than one we've already calculated, then replace it. Otherwise just store it.
-          if new_path.move_distance <= movement_points
-            old_path = open_paths[testing_tile]
-            if old_path
-              if new_path.move_distance < old_path.move_distance
-                open_paths[testing_tile] = new_path
-              end
-            else
-              open_paths[testing_tile] = new_path
-              valid_tiles << testing_tile if testing_tile.empty?
-            end
-          end
-        end
-      end
-    end
-
-    valid_tiles
-  end
-
-  # A* path-finding.
-  def path_to(destination_tile)
-    return Paths::None.new if destination_tile == tile
-    return Paths::Inaccessible.new(destination_tile) unless destination_tile.passable?(self)
-
-    closed_tiles = Set.new # Tiles we've already dealt with.
-    open_paths = { tile => Paths::Start.new(tile, destination_tile) } # Paths to check { tile => path_to_tile }.
-
-    destination_object =  destination_tile.object
-    destination_is_enemy = (destination_object and destination_object.is_a? Entity and destination_object.enemy?(self))
-
-    melee_cost = has_ability?(:melee) ? ability(:melee).action_cost : Float::INFINITY
-
-    while open_paths.any?
-      # Check the (expected) shortest path and move it to closed, since we have considered it.
-      path = open_paths.each_value.min_by(&:cost)
-      current_tile = path.last
-
-      return path if current_tile == destination_tile
-
-      open_paths.delete current_tile
-      closed_tiles << current_tile
-
-      next if path.is_a? Paths::Melee
-
-      # Check adjacent tiles.
-      exits = current_tile.exits(self).reject {|wall| closed_tiles.include? wall.destination(current_tile) }
-      exits.each do |wall|
-        testing_tile = wall.destination(current_tile)
-
-        new_path = nil
-
-        object = testing_tile.object
-        if testing_tile.zoc?(faction) and not (testing_tile == destination_tile or destination_is_enemy)
-          # Avoid tiles that have zoc, unless at the end of the path. You have to MANUALLY enter.
-          next
-        elsif object and object.is_a?(Objects::Entity) and enemy?(object)
-          # Ensure that the current tile is somewhere we could launch an attack from and we could actually perform it.
-          if (current_tile.empty? or current_tile == tile) and ap >= melee_cost
-            new_path = Paths::Melee.new(path, testing_tile)
-          else
-            next
-          end
-        elsif testing_tile.passable?(self)
-          if object.nil? or object.passable?(self)
-            new_path = Paths::Move.new(path, testing_tile, wall.movement_cost)
-          else
-            next
-          end
-        end
-
-        # If the path is shorter than one we've already calculated, then replace it. Otherwise just store it.
-        old_path = open_paths[testing_tile]
-        if old_path
-          if new_path.move_distance < old_path.move_distance
-            open_paths[testing_tile] = new_path
-          end
-        else
-          open_paths[testing_tile] = new_path
-        end
-      end
-    end
-
-    Paths::Inaccessible.new(destination_tile) # Failed to connect at all.
-  end
-
   def update
     super
 
@@ -561,72 +447,6 @@ class Entity < WorldObject
     end
   end
 
-  #
-  def line_of_sight?(tile)
-    !line_of_sight_blocked_by(tile)
-  end
-
-  # Returns the tile that blocks sight, otherwise nil.
-  # Implements 'Bresenham's line algorithm'
-  def line_of_sight_blocked_by(target_tile)
-    start_tile = tile
-
-    # Check for the special case of looking diagonally.
-    x1, y1 = tile.grid_x, tile.grid_y
-    x2, y2 = target_tile.grid_x, target_tile.grid_y
-
-    step_x = x1 < x2 ? 1 : -1
-    step_y = y1 < y2 ? 1 : -1
-    dx, dy = (x2 - x1).abs, (y2 - y1).abs
-
-    if dx == dy
-      # Special case of the diagonal line.
-      (dx - 1).times do
-        x1 += step_x
-        y1 += step_y
-
-        # If the centre tile is blocked, then we don't work.
-        tile = @map.tile_at_grid(x1, y1)
-        if tile.blocks_sight?
-          #Tile.blank.draw_rot tile.x, tile.y, ZOrder::TILE_SELECTION, 0, 0.5, 0.5, 1, 1, Color::RED
-          return tile
-        else
-          #Tile.blank.draw_rot tile.x, tile.y, ZOrder::TILE_SELECTION, 0, 0.5, 0.5, 1, 1, Color::BLUE
-        end
-      end
-    else
-      # General case, ray-trace.
-      error = dx - dy
-
-      # Ensure that all tiles are visited that the sight-line passes over,
-      # not just those that create a "drawn" line.
-      dx *= 2
-      dy *= 2
-
-      length = ((dx + dy + 1) / 2)
-
-      (length - 1).times do
-        # Note that this ignores the special case of error == 0
-        if error > 0
-          error -= dy
-          x1 += step_x
-        else
-          error += dx
-          y1 += step_y
-        end
-
-        tile = @map.tile_at_grid(x1, y1)
-        if tile.blocks_sight?
-          #Tile.blank.draw_rot tile.x, tile.y, ZOrder::TILE_SELECTION, 0, 0.5, 0.5, 1, 1, Color::RED
-          return tile
-        else
-          #Tile.blank.draw_rot tile.x, tile.y, ZOrder::TILE_SELECTION, 0, 0.5, 0.5, 1, 1, Color::BLUE
-        end
-      end
-    end
-
-    nil # Didn't hit anything.
-  end
 
   def use_ability(name, *args)
     raise "#{self} does not have ability: #{name.inspect}" unless has_ability? name
